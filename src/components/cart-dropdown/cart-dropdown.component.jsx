4246d7b79b0d536861eb0c14d66d0c30d4d9af45
unknown <sakshi.rdkar@gmail.com>
2021-06-11 18:22:26 +0530

    Created cart item component and display cartitem to dropdoen

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ButtonCustom from './../Button/button.component';
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+
+const CartDropdown = ({ cartItems}) => (
     
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items' >
+        {cartItems.map((item) => (
+            <CartItem id={item.id} item={ item} />
+        ))}
+        </div>
         <ButtonCustom  type= 'button'> GO TO CHECKOUT </ButtonCustom>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart: { cartItems }}) => ({
+    cartItems
+});
+
+export default  connect(mapStateToProps) (CartDropdown);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -28,4 +28,4 @@ const mapDispatchToProps = (dispatch) => (
     addItem: (item) => dispatch(addItem(item))
     }
 );
-export default connect(null,mapDispatchToProps) (CollectionItem);
+export default connect(mapDispatchToProps) (CollectionItem);
